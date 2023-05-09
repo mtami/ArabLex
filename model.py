7c47b37fe9d8bff0868a3c0b5896da1f68462c3f
tami <mabutami@apple.com>
2023-05-09 14:09:11 +0300

    cache model load

--- model.py
+++ model.py
@@ -6,26 +6,39 @@ import shutil
 import streamlit as st
 import gensim
 
+import tempfile
+
 
 @st.experimental_memo
 def download_fastext():
+    # Create a temporary directory
+    temp_dir = tempfile.mkdtemp()
+
     # Download the Arabic FastText model
     url = "https://dl.fbaipublicfiles.com/fasttext/vectors-crawl/cc.ar.300.bin.gz"
     response = requests.get(url, stream=True)
-    with open("cc.ar.300.bin.gz", "wb") as file:
+    gz_path = os.path.join(temp_dir, "cc.ar.300.bin.gz")
+    with open(gz_path, "wb") as file:
         for chunk in response.iter_content(chunk_size=8192):
             file.write(chunk)
 
     # Extract the .bin file from the .gz archive
-    with gzip.open("cc.ar.300.bin.gz", "rb") as f_in:
-        with open("cc.ar.300.bin", "wb") as f_out:
+    bin_path = os.path.join(temp_dir, "cc.ar.300.bin")
+    with gzip.open(gz_path, "rb") as f_in:
+        with open(bin_path, "wb") as f_out:
             shutil.copyfileobj(f_in, f_out)
 
+    return bin_path
 
-if not os.path.exists("./cc.ar.300.bin"):
-    download_fastext()
 
-model = gensim.models.fasttext.load_facebook_vectors('./cc.ar.300.bin')
+@st.cache_resource
+def load_fastext():
+    bin_path = download_fastext()
+    model = gensim.models.fasttext.load_facebook_vectors(bin_path)
+    return model
+
+
+model = load_fastext()
 
 
 def get_top_k(word, k=10):
